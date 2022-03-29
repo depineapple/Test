a7441e40ea5ea7371feb50da893dee8c679fa0d4
Никита Коваль <nikita.koval.01@bk.ru>
2022-03-29 15:44:33 +1000

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.Json;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using ConsoleApp1;
+using System.Linq;
+
+var path = "https://raw.githubusercontent.com/thewhitesoft/student-2022-assignment/main/data.json";
+HttpClient client = new HttpClient();
+client.BaseAddress = new Uri(path);
+Replacement storage = new Replacement();
+List<Replacement> replacement;       
+var streamTask = client.GetStreamAsync(path);
+List<string> storages = await JsonSerializer.DeserializeAsync<List<string>>(await streamTask);
+using (FileStream fs = new FileStream("replacement.json", FileMode.OpenOrCreate))
+{
+    replacement = await JsonSerializer.DeserializeAsync<List<Replacement>>(fs);
+}
+string n = "";
+List<string> result = new List<string>();
+foreach (var s in storages)
+{
+    n = s;
+    List<string> words = new List<string>();
+    List<Replacement> replacements = replacement.FindAll(r => s.Contains(r.replacement));
+    if (replacements.Count > 1)
+    {
+        for (int j = 0; j < replacements.Count - 1; j++)
+        {
+            if (replacements[j].replacement.Contains(replacements[j + 1].replacement) || replacements[j + 1].replacement.Contains(replacements[j].replacement))
+                if (replacements[j].replacement.Length > replacements[j + 1].replacement.Length)
+                    replacements.RemoveAt(j + 1);
+                else replacements.RemoveAt(j);
+        }
+        for (int j = 0; j < replacements.Count - 1; j++)
+        {
+            if (replacements[j].replacement == replacements[j + 1].replacement && replacements[j].source == replacements[j + 1].source)
+                replacements.RemoveAt(j);
+        }
+        for (int j = 0; j < replacements.Count - 1; j++)
+        {
+            if (replacements[j].replacement == replacements[j + 1].replacement && replacements[j].source != replacements[j + 1].source)
+                replacements.RemoveAt(j);
+        }
+       
+    }
+    foreach (var rep in replacements)
+    {
+        n = n.Replace(n, n.Replace(rep.replacement, rep.source));
+    }
+    if (n != "")
+    {
+        Console.WriteLine(n);
+        result.Add(n);
+    }
+}
+using (FileStream fs = new FileStream("result.json", FileMode.OpenOrCreate))
+{
+    var options = new JsonSerializerOptions
+    {
+        WriteIndented = true
+
+    };
+    await JsonSerializer.SerializeAsync<List<string>>(fs, result, options);
+    Console.WriteLine();
+    Console.WriteLine("Данные были сериализованы");
+}
---
+++ ConsoleApp1/Storage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Replacement
+    {
+        public string replacement { get; set; }
+        public string source { get; set; }
+        //public string[] vs { get; set; }
+    }
+}
