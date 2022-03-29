9e803646f616237d5ceaef9ac0ac5c7746cfc3df
Никита Коваль <nikita.koval.01@bk.ru>
2022-03-29 16:01:41 +1000

    Commit

---
+++ ConsoleApp1/JsonMethods.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+
+namespace ConsoleApp1
+{
+    internal class JsonMethods
+    {
+        string pathDataJson = "https://raw.githubusercontent.com/thewhitesoft/student-2022-assignment/main/data.json";
+        string pathReplacementJson = "replacement.json";
+        HttpClient httpClient = new HttpClient();
+
+        public async Task<List<string>> getDataJsonList()
+        {
+
+            httpClient.BaseAddress = new Uri(pathDataJson);
+            var streamTask = httpClient.GetStreamAsync(pathDataJson);
+            var dataList = await JsonSerializer.DeserializeAsync<List<string>>(await streamTask);           
+            return dataList;
+        }
+        public async Task<List<Replacement>> getReplacementJsonListAsync()
+        {
+            List<Replacement> replacements;
+            using (FileStream fs = new FileStream(pathReplacementJson, FileMode.OpenOrCreate))
+            {
+                replacements = await JsonSerializer.DeserializeAsync<List<Replacement>>(fs);
+            }
+            return replacements;
+        }
+
+        async Task resultToJsonAsync(List<string> results)
+        {
+            using (FileStream fs = new FileStream("result.json", FileMode.OpenOrCreate))
+            {
+                var options = new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                };
+                await JsonSerializer.SerializeAsync(fs, results, options);
+                Console.WriteLine();
+                Console.WriteLine("Данные были сериализованы");
+            }
+        }
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,68 +1,48 @@
-﻿using System.Text.Json;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using ConsoleApp1;
-using System.Linq;
+﻿using ConsoleApp1;
 
-var path = "https://raw.githubusercontent.com/thewhitesoft/student-2022-assignment/main/data.json";
-HttpClient client = new HttpClient();
-client.BaseAddress = new Uri(path);
-Replacement storage = new Replacement();
-List<Replacement> replacement;       
-var streamTask = client.GetStreamAsync(path);
-List<string> storages = await JsonSerializer.DeserializeAsync<List<string>>(await streamTask);
-using (FileStream fs = new FileStream("replacement.json", FileMode.OpenOrCreate))
-{
-    replacement = await JsonSerializer.DeserializeAsync<List<Replacement>>(fs);
-}
-string n = "";
-List<string> result = new List<string>();
-foreach (var s in storages)
-{
-    n = s;
-    List<string> words = new List<string>();
-    List<Replacement> replacements = replacement.FindAll(r => s.Contains(r.replacement));
-    if (replacements.Count > 1)
-    {
-        for (int j = 0; j < replacements.Count - 1; j++)
-        {
-            if (replacements[j].replacement.Contains(replacements[j + 1].replacement) || replacements[j + 1].replacement.Contains(replacements[j].replacement))
-                if (replacements[j].replacement.Length > replacements[j + 1].replacement.Length)
-                    replacements.RemoveAt(j + 1);
-                else replacements.RemoveAt(j);
-        }
-        for (int j = 0; j < replacements.Count - 1; j++)
-        {
-            if (replacements[j].replacement == replacements[j + 1].replacement && replacements[j].source == replacements[j + 1].source)
-                replacements.RemoveAt(j);
-        }
-        for (int j = 0; j < replacements.Count - 1; j++)
-        {
-            if (replacements[j].replacement == replacements[j + 1].replacement && replacements[j].source != replacements[j + 1].source)
-                replacements.RemoveAt(j);
-        }
-       
-    }
-    foreach (var rep in replacements)
-    {
-        n = n.Replace(n, n.Replace(rep.replacement, rep.source));
-    }
-    if (n != "")
-    {
-        Console.WriteLine(n);
-        result.Add(n);
-    }
-}
-using (FileStream fs = new FileStream("result.json", FileMode.OpenOrCreate))
-{
-    var options = new JsonSerializerOptions
-    {
-        WriteIndented = true
+JsonMethods jsonMethods = new JsonMethods();
+List<string> resultsList = new List<string>(), dataList = new List<string>();
+List<Replacement> replacementsList;
+string? rawData = null;
 
-    };
-    await JsonSerializer.SerializeAsync<List<string>>(fs, result, options);
-    Console.WriteLine();
-    Console.WriteLine("Данные были сериализованы");
+
+dataList = jsonMethods.getDataJsonList().Result;
+replacementsList = jsonMethods.getReplacementJsonListAsync().Result;
+doReplacements();
+
+void doReplacements()
+{
+    foreach (var dataRaw in dataList)
+    {
+        replacementsList = replacementsList.FindAll(r => dataRaw.Contains(r.replacement));
+        if (replacementsList.Count > 1)
+        {
+            for (int j = 0; j < replacementsList.Count - 1; j++)
+            {
+                if (replacementsList[j].replacement.Contains(replacementsList[j + 1].replacement) || replacementsList[j + 1].replacement.Contains(replacementsList[j].replacement))
+                    if (replacementsList[j].replacement.Length > replacementsList[j + 1].replacement.Length)
+                        replacementsList.RemoveAt(j + 1);
+                    else replacementsList.RemoveAt(j);
+            }
+            for (int j = 0; j < replacementsList.Count - 1; j++)
+            {
+                if (replacementsList[j].replacement == replacementsList[j + 1].replacement && replacementsList[j].source == replacementsList[j + 1].source)
+                    replacementsList.RemoveAt(j);
+            }
+            for (int j = 0; j < replacementsList.Count - 1; j++)
+            {
+                if (replacementsList[j].replacement == replacementsList[j + 1].replacement && replacementsList[j].source != replacementsList[j + 1].source)
+                    replacementsList.RemoveAt(j);
+            }
+        }
+        foreach (var rep in replacementsList)
+        {
+            rawData = dataRaw.Replace(dataRaw, dataRaw.Replace(rep.replacement, rep.source));
+        }
+        if (dataRaw != "")
+        {
+            Console.WriteLine(dataRaw);
+            resultsList.Add(dataRaw);
+        }
+    }
 }
